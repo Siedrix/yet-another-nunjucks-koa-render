dee8350303ac4653810cf2e91fd606e07669e4c3
Daniel Zavala <siedrix@gmail.com>
2016-08-28 11:00:33 -0600

    First

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # yet-another-nunjucks-koa-render
 Allows nunjucks templates to koa 2.0
+
+Create you nunjucks env and pass it to the render. It will be added to Koa
+```
+const Koa = require('koa')
+const nunjucks = require('nunjucks')
+const render = require('yet-another-nunjucks-koa-render')
+
+const server = new Koa()
+
+app.use( render(nunjucks.configure('./views'), {ext:'.html'}) )
+```
---
+++ render.js
@@ -0,0 +1,24 @@
+const co = require('co')
+const _ = require('lodash')
+
+module.exports = function(nunjucks, opts) {
+	opts = opts || {}
+	opts.ext = opts.ext || ''
+
+	return co.wrap(function*(ctx, next) {
+		ctx.render = function(view, context) {
+			return new Promise(function(resolve, reject) {
+				_.merge(context, ctx.state)
+				
+				nunjucks.render(view+ opts.ext, context, function(err, body) {
+					if (err){ return reject(err) }
+
+					ctx.body = body;
+					resolve();
+				});
+			});
+		};
+
+		yield next();
+	})
+}
